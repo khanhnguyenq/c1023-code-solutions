e629d280dfae4b1479cd16885d9553b5022b0e18
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2023-10-23 21:27:46 +0000

    Completed javascript-operators-and-expressions exercise

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,46 @@
+const width = 10;
+const height = 10;
+const area = width * height;
+
+console.log('value of area: ', area);
+console.log('typeof area: ', typeof area);
+
+const bill = 100;
+const payment = 67;
+const change = bill - payment;
+
+console.log('value of change: ', change);
+console.log('typeof change: ', typeof change);
+
+const quizzes = 89;
+const midterm = 92;
+const final = 95;
+const grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade: ', grade);
+console.log('typeof grade: ', typeof grade);
+
+const firstName = 'Khanh';
+const lastName = 'Nguyen';
+const fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName: ', fullName);
+console.log('typeof fullName: ', typeof fullName);
+
+const pH = 7;
+const isAcidic = pH < 7;
+
+console.log('value of isAcidic: ', isAcidic);
+console.log('typeof isAcidic: ', typeof isAcidic);
+
+const headCount = 12;
+const isSparta = headCount === 300;
+
+console.log('value of isSparta: ', isSparta);
+console.log('typeof isSparta: ', typeof isSparta);
+
+let motto = fullName;
+motto += ' is the GOAT';
+
+console.log('value of motto: ', motto);
+console.log('typeof motto: ', typeof motto);
