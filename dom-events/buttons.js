9440a8ce117da0e0e33c8ba63d714943deb05b09
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2023-11-06 21:39:17 +0000

    dom-events - Khanh Nguyen

---
+++ dom-events/buttons.js
@@ -0,0 +1,29 @@
+function handleClick(event) {
+  console.log('button clicked');
+  console.log('event', event);
+  console.log('target of event', event.target);
+}
+
+const $clickButton = document.querySelector('.click-button');
+
+$clickButton.addEventListener('click', handleClick);
+
+function handleMouseover(event) {
+  console.log('button hovered');
+  console.log('event:', event);
+  console.log('target of event:', event.target);
+}
+
+const $hoverButton = document.querySelector('.hover-button');
+
+$hoverButton.addEventListener('mouseover', handleMouseover);
+
+function handleDoubleClick(event) {
+  console.log('button double-clicked');
+  console.log('event:', event);
+  console.log('target of event:', event.target);
+}
+
+const $doubleClick = document.querySelector('.double-click-button');
+
+$doubleClick.addEventListener('dblclick', handleDoubleClick);
