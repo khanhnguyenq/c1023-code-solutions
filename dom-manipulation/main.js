6992f06aa954eb73bbeaf8dcd939cc21ad2bab77
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2023-11-06 23:48:23 +0000

    updated comparison from <= to < - Khanh Nguyen

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -6,15 +6,15 @@ const $clickCount = document.querySelector('.click-count');
 $hotButton.addEventListener('click', function (event) {
   count++;
   $clickCount.textContent = `Clicks: ${count}`;
-  if (count <= 4) {
+  if (count < 4) {
     $hotButton.className = 'hot-button cold';
-  } else if (count <= 7) {
+  } else if (count < 7) {
     $hotButton.className = 'hot-button cool';
-  } else if (count <= 10) {
+  } else if (count < 10) {
     $hotButton.className = 'hot-button tepid';
-  } else if (count <= 13) {
+  } else if (count < 13) {
     $hotButton.className = 'hot-button warm';
-  } else if (count <= 16) {
+  } else if (count < 16) {
     $hotButton.className = 'hot-button hot';
   } else {
     $hotButton.className = 'hot-button nuclear';
