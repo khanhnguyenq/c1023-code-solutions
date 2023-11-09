6672d53b072ccdbb120f89402915118e6a8c0e1e
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2023-11-09 19:46:20 +0000

    dom-event-delegation - Khanh Nguyen

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -4,8 +4,8 @@ $taskList.addEventListener('click', function (event) {
   console.log('event.target', event.target);
   console.log('event.target.tagName:', event.target.tagName);
   if (event.target.tagName.toLowerCase() === 'button') {
-    const closestItem = event.target.closest('.task-list-item');
-    console.log('closest:', closestItem);
-    closestItem.remove();
+    const $closestItem = event.target.closest('.task-list-item');
+    console.log('closest:', $closestItem);
+    $closestItem.remove();
   }
 });
