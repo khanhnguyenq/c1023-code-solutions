33a123bf82e8426d686340ba4d45848c7fe18d6b
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2023-12-06 23:02:25 +0000

    typescript-primitives-and-variables - Khanh Nguyen

---
+++ typescript-primitives-and-variables/main.ts
@@ -0,0 +1,24 @@
+const fullName: string = 'Khanh';
+
+const isCool: boolean = true;
+
+const totalPets: number = 1;
+
+console.log('value of fulllName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+console.log('value of isCool:', isCool);
+console.log('typeof isCool:', typeof isCool);
+
+console.log('value of totalPets:', totalPets);
+console.log('typeof totalPets:', typeof totalPets);
+
+const unknown: null = null;
+
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+const never: undefined = undefined;
+
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
