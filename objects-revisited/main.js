3f33b6bd0c2c8f925f849edf3b13ce7ce2bbbaf4
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2023-12-07 00:49:31 +0000

    updated code into smaller functions and pending deletion functionality

--- objects-revisited/main.js
+++ objects-revisited/main.js
@@ -30,57 +30,65 @@ function addWeekends() {
   }
 }
 
+const workDay = [];
+const title = ['React.JS', 'MongoDB', 'Express', 'TypeScript'];
+const numNew = 4;
+
+function createSchedule() {
+  const daysOpen = ['M', 'T', 'W', 'Th', 'F'];
+  const schedule = [];
+  for (let i = 0; i < numNew; i++) {
+    schedule.push(Math.floor(Math.random() * 5 + 1));
+  }
+  for (let i = 0; i < numNew; i++) {
+    workDay.push([]);
+  }
+  for (let i = 0; i < schedule.length; i++) {
+    for (let k = 0; k < schedule[i]; k++) {
+      const day = daysOpen[Math.floor(Math.random() * 4)];
+      if (!workDay[i].includes(day)) {
+        workDay[i].push(day);
+      }
+    }
+  }
+}
+
+function checkFTUpdateTotal() {
+  let total = 0;
+  for (const x in business.employees) {
+    business.employees[x].fullTime =
+      business.employees[x].daysOfWeekWorking.length >= 5;
+    total += 1;
+  }
+  business.totalEmployees = total;
+}
+
 function addEmployees() {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
   xhr.responseType = 'json';
   xhr.addEventListener('load', () => {
-    const daysOpen = ['M', 'T', 'W', 'Th', 'F'];
-    const numNew = 4;
-    const title = ['React.JS', 'MongoDB', 'Express', 'TypeScript'];
-    const schedule = [];
+    createSchedule();
     for (let i = 0; i < numNew; i++) {
-      schedule.push(Math.floor(Math.random() * 5 + 1));
-    }
-    const workDay = [];
-    for (let i = 0; i < numNew; i++) {
-      workDay.push([]);
-    }
-    for (let i = 0; i < schedule.length; i++) {
-      for (let k = 0; k < schedule[i]; k++) {
-        const day = daysOpen[Math.floor(Math.random() * 4)];
-        if (!workDay[i].includes(day)) {
-          workDay[i].push(day);
-        }
-      }
-    }
-    for (let i = 0; i < numNew; i++) {
-      business.employees[xhr.response[i].name] = {
+      let fullName = xhr.response[i].name.toLowerCase().split(' ');
+      let firstName = fullName[0];
+      business.employees[firstName] = {
         position: title[i],
         daysOfWeekWorking: ['Su', ...workDay[i], 'S'],
       };
     }
-    let total = 0;
-    for (const x in business.employees) {
-      business.employees[x].fullTime =
-        business.employees[x].daysOfWeekWorking.length >= 5;
-      total += 1;
-    }
-    business.totalEmployees = total;
+    checkFTUpdateTotal();
   });
   xhr.send();
 }
 
 function deleteEmployee(employeeName) {
-  console.log('business obj:', business);
-  // business.totalEmployees = business.totalEmployees - 1;
-  console.log('employee obj', business.employees[employeeName]);
   delete business.employees[employeeName];
+  checkFTUpdateTotal();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   addWeekends();
   addEmployees();
-  deleteEmployee('dan');
-  // console.log('business:', business);
+  deleteEmployee('ervin');
 });
