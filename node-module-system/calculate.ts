300f0003abc23f85953894d69d96068f70b5015b
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2023-12-12 22:28:00 +0000

    updated code and added instructor's answer

--- node-module-system/calculate.ts
+++ node-module-system/calculate.ts
@@ -3,26 +3,69 @@ import { difference } from './subtract.js';
 import { multiply } from './multiply.js';
 import { divide } from './divide.js';
 
-const operation: string = process.argv[3];
+const operation = process.argv[3];
+const arg1 = +process.argv[2];
+const arg2 = +process.argv[4];
+
+let result: number;
+
+if (Number.isNaN(arg1)) {
+  console.log(`${process.argv[2]} is not a number.`);
+  process.exit(1);
+}
+
+if (Number.isNaN(arg2)) {
+  console.log(`${process.argv[4]} is not a number.`);
+  process.exit(1);
+}
 
 if (operation === 'plus') {
-  console.log(
-    'result:',
-    sum(parseInt(process.argv[2]), parseInt(process.argv[4]))
-  );
+  result = sum(arg1, arg2);
 } else if (operation === 'minus') {
-  console.log(
-    'result:',
-    difference(parseInt(process.argv[2]), parseInt(process.argv[4]))
-  );
+  result = difference(arg1, arg2);
 } else if (operation === 'times') {
-  console.log(
-    'result:',
-    multiply(parseInt(process.argv[2]), parseInt(process.argv[4]))
-  );
+  result = multiply(arg1, arg2);
 } else if (operation === 'divide') {
-  console.log(
-    'result:',
-    divide(parseInt(process.argv[2]), parseInt(process.argv[4]))
-  );
+  result = divide(arg1, arg2);
+} else {
+  console.log('invalid operation');
+  process.exit(1);
 }
+
+console.log('result:', result);
+
+// instructor's answer
+// import { add } from './add.js';
+// import { subtract } from './subtract.js';
+// import { multiply } from './multiply.js';
+// import { divide } from './divide.js';
+// const arg1 = +process.argv[2];
+// const operator = process.argv[3];
+// const arg2 = +process.argv[4];
+// if (Number.isNaN(arg1)) {
+//   console.log(`${process.argv[2]} is not a number`);
+//   process.exit(1);
+// }
+// if (Number.isNaN(arg2)) {
+//   console.log(`${process.argv[4]} is not a number`);
+//   process.exit(1);
+// }
+// let result: number;
+// switch (operator) {
+//   case 'plus':
+//     result = add(arg1, arg2);
+//     break;
+//   case 'minus':
+//     result = subtract(arg1, arg2);
+//     break;
+//   case 'times':
+//     result = multiply(arg1, arg2);
+//     break;
+//   case 'over':
+//     result = divide(arg1, arg2);
+//     break;
+//   default:
+//     console.log('invalid operator');
+//     process.exit(1);
+// }
+// console.log('result:', result);
