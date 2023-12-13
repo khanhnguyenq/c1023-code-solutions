69fdde733bbfe48632cae380f55fa873191cbe90
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2023-12-13 22:39:11 +0000

    node-fs-readfile - Khanh Nguyen

---
+++ node-fs-readfile/cat.ts
@@ -0,0 +1,17 @@
+import fs from 'node:fs/promises';
+
+async function readSelected(): Promise<void> {
+  try {
+    const inputs = process.argv.slice(2);
+    const concatFiles = inputs.map((file) => fs.readFile(`./${file}`, 'utf-8'));
+    console.log(
+      'Concatenated Files:',
+      '\n',
+      (await Promise.all(concatFiles)).toString()
+    );
+  } catch (err) {
+    console.log('Error:', err);
+  }
+}
+
+readSelected();
---
+++ node-fs-readfile/read-any.ts
@@ -0,0 +1,13 @@
+import fs from 'node:fs/promises';
+
+async function read(): Promise<void> {
+  try {
+    const fileName = process.argv[2];
+    const content = await fs.readFile(`./${fileName}`, 'utf-8');
+    console.log('Content of file:', '\n', content);
+  } catch (err) {
+    console.log('Error:', err);
+  }
+}
+
+read();
