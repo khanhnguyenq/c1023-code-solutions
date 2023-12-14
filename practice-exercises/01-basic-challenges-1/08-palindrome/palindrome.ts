03a69d92d44cef3dfe690aac96b7dd8223b64dd5
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2023-12-14 17:07:53 +0000

    completed 01-07. 01-08 in progess

--- practice-exercises/01-basic-challenges-1/08-palindrome/palindrome.ts
+++ practice-exercises/01-basic-challenges-1/08-palindrome/palindrome.ts
@@ -1,3 +1,20 @@
 export function isPalindrome(str: string): boolean {
-  return false;
+  const newStr = str.toLowerCase().split('');
+  for (let i = 0; i < newStr.length; i++) {
+    if (newStr[i] === ' ' && newStr[i] === ',') {
+      newStr.splice(i, 1);
+    }
+  }
+  let left: number = 0;
+  let right: number = newStr.length - 1;
+
+  for (let i = 0; i < newStr.length; i++) {
+    if (newStr[left] !== newStr[right]) {
+      return false;
+    } else {
+      left++;
+      right--;
+    }
+  }
+  return true;
 }
