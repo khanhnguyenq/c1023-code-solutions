9900ee91582c8bd73e298e59273fe6463aa338a1
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2023-12-15 20:06:07 +0000

    express-delete - Khanh Nguyen

---
+++ express-delete/server.ts
@@ -0,0 +1,48 @@
+import express from 'express';
+
+const app = express();
+
+type Grade = {
+  id: number;
+  name: string;
+  course: string;
+  score: number;
+};
+
+const grades: Record<number, Grade> = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95,
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100,
+  },
+  273: {
+    id: 273,
+    name: 'Håkon Wium Lie',
+    course: 'CSS',
+    score: 92,
+  },
+};
+
+app.get('/api/grades', (req, res) => {
+  const gradesArray: Grade[] = [];
+  for (const x in grades) {
+    gradesArray.push(grades[x]);
+  }
+  res.json(gradesArray);
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  delete grades[+req.params];
+  res.sendStatus(204);
+});
+
+app.listen(8080, () => {
+  console.log('Listening on 8080');
+});
