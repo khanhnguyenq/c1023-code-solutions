0fe8d025a346b85ad458e27fb1cc34361ea0351b
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2023-12-17 06:01:48 +0000

    completed user can get single note by id

--- express-notes-api/server.ts
+++ express-notes-api/server.ts
@@ -6,17 +6,36 @@ type Note = {
   note: string;
 };
 
+type Data = {
+  nextId: number;
+  notes: Record<string, Note>;
+};
+
 const app = express();
 app.use(express.json());
 
+async function read(): Promise<Data> {
+  const content = await readFile('data.json', 'utf8');
+  return JSON.parse(content);
+}
+
 app.get('/api/notes', async (req, res) => {
-  const content: string = await readFile('data.json', 'utf8');
-  const contentObj = JSON.parse(content);
+  const contentObj = await read();
   const notesArray: Note[] = [];
   for (const x in contentObj.notes) {
     notesArray.push(contentObj.notes[x]);
   }
   res.json(notesArray);
+});
+
+app.get('/api/notes/:id', async (req, res) => {
+  const contentObj = await read();
+  const id = req.params.id;
+  if (!contentObj.notes[id]) {
+    res.status(404).send('ID does not exist');
+  } else {
+    res.json(contentObj.notes[id]);
+  }
 });
 
 app.listen(8080, () => {
