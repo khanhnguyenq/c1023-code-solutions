be0b01aa6b07ca1c5efe8e8516264d1ee810c8a1
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2023-12-19 19:02:55 +0000

    pending 0207 exercise completion

--- practice-exercises/02-basic-challenges-2/08-dice-game/dice-game.ts
+++ practice-exercises/02-basic-challenges-2/08-dice-game/dice-game.ts
@@ -7,5 +7,29 @@ export type Simulation = {
 };
 
 export function diceGameSimulation(numSimulations: number): Simulation[] {
-  return [];
+  const resultsArray: Simulation[] = [];
+  function resultMessage(num: number): string {
+    switch (num) {
+      case 7:
+      case 11:
+        return 'win';
+      case 2:
+      case 3:
+      case 12:
+        return 'lose';
+      default:
+        return 'roll again';
+    }
+  }
+  for (let i = 0; i < numSimulations; i++) {
+    const resultObj: Simulation = {};
+    const dice1 = Math.floor(Math.random() * 6) + 1;
+    const dice2 = Math.floor(Math.random() * 6) + 1;
+    const sum = dice1 + dice2;
+    const result = resultMessage(sum);
+    resultObj.dice1 = dice1;
+
+    resultsArray.push(resultObj);
+  }
+  return resultsArray;
 }
