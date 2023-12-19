81fc37d30ca78f4e08301e19b920b4355968e412
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2023-12-19 19:01:35 +0000

    pending 0207 exercise completion

--- practice-exercises/02-basic-challenges-2/07-first-non-repeating/first-non-repeating.ts
+++ practice-exercises/02-basic-challenges-2/07-first-non-repeating/first-non-repeating.ts
@@ -1,5 +1,10 @@
 export function findFirstNonRepeatingCharacter(
   str: string
 ): string | undefined {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[i - 1] && str[i] !== str[i + 1]) {
+      return str[i];
+    }
+  }
   return undefined;
 }
