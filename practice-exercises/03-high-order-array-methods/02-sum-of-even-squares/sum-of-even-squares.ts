868f6e3df6ae9ce91f8e96cfc9325f00111c99ae
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2023-12-22 00:15:59 +0000

    completed 03-02

--- practice-exercises/03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.ts
+++ practice-exercises/03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.ts
@@ -1,3 +1,5 @@
 export function sumOfEvenSquares(numbers: number[]): number {
-  return 0;
+  const evens = numbers.filter((x) => x % 2 === 0);
+  const squared = evens.map((x) => x * x);
+  return squared.reduce((total, num) => total + num, 0);
 }
