a35bef3bb0cde2a6a002c67a7822a635c31ac158
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2024-01-08 16:42:01 +0000

    pending exercise completion

---
+++ javascript-closures/my-app/src/Counter.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react-hooks/exhaustive-deps -- Delete this line! */
+import { useEffect, useState } from 'react';
+
+export function Counter() {
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    getData();
+    setCounter((prev) => prev + 1);
+  }, [getData]);
+
+  function getData() {
+    // fetch data
+    return { foo: 'bar' };
+  }
+
+  return <div>Fetching {counter} times</div>;
+}
