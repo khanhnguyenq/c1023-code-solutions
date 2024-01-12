eb5512b971d5599a8484bee569aed04b6fa8f1fe
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2024-01-12 18:17:03 +0000

    updated code based on PR review

--- react-routing/client/src/App.tsx
+++ react-routing/client/src/App.tsx
@@ -7,15 +7,13 @@ import { Header } from './components/Header';
 
 export function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Header />}>
-          <Route index element={<Catalog />} />
-          <Route path="details/:productId" element={<ProductDetails />} />
-          <Route path="about" element={<About />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Catalog />} />
+        <Route path="details/:productId" element={<ProductDetails />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
